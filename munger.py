d462963c12901f2e23c9f304ca3ecd69cedca6d3
Douglas Storz <douglasstorz@gmail.com>
2022-04-19 12:08:34 -0400

    Abstracted Processor to its own file

--- munger.py
+++ munger.py
@@ -28,10 +28,9 @@ import csv
 from enum import Enum, auto
 from pathlib import Path
 
-# I rename this to Processor to avoid confusion with the validator schema type processor.
-# And besides, I'm abusing cerberus so much already it's far past being just a Validator.
-from cerberus import Validator as Processor
 from tqdm import tqdm
+
+from processor import Processor
 
 
 class MungeFailureException(Exception):
---
+++ processor.py
@@ -0,0 +1,3 @@
+# I rename this to Processor to avoid confusion with the validator schema type processor.
+# And besides, I'm abusing cerberus so much already it's far past being just a Validator.
+from cerberus import Validator as Processor
